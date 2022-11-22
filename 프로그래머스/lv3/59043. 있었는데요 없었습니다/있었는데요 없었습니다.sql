3b788d92fd776f255a8328d06bba0d55557b4f34
shjeon22 <107968231+shjeon22@users.noreply.github.com>
2022-11-22 11:29:48 +0900

    Update 있었는데요 없었습니다.sql

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,5 +1,38 @@
+EX1 (ON절 이용) > (ANSI JOIN) 국제적 표준 조인 방법
+
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_INS A  INNER JOIN ANIMAL_OUTS  B  --INNER JOIN(명시적) =JOIN
+ON A.ANIMAL_ID = B.ANIMAL_ID
+WHERE A.DATETIME > B.DATETIME  
+ORDER BY A.DATETIME ASC;
+
+-------------------------------------------------------------------------------------------
+
+EX 2.(USING 이용) > (ANSI JOIN) 국제적 표준 조인 방법
+
+SELECT ANIMAL_ID, A.NAME
+FROM ANIMAL_INS A  INNER JOIN ANIMAL_OUTS B 
+USING(ANIMAL_ID)
+WHERE A.DATETIME > B.DATETIME
+ORDER BY A.DATETIME
+
+-------------------------------------------------------------------------------------------
+
+EX 3> 암묵적 조인 (FROM절에 JOIN 생략 쉼표(,)구분 , 및 ON,USING절 => WHERE절에 명시 )
+
 SELECT A.ANIMAL_ID, A.NAME  
-FROM ANIMAL_INS A , ANIMAL_OUTS B 
+FROM ANIMAL_INS A,ANIMAL_OUTS B 
 WHERE A.ANIMAL_ID = B.ANIMAL_ID
 AND A.DATETIME > B.DATETIME
-ORDER BY A.DATETIME ASC;
+ORDER BY A.DATETIME;
+
+-------------------------------------------------------------------------------------------
+
+EX 4> Subquery -서브쿼리 이용
+
+SELECT A.ANIMAL_ID, A.NAME
+FROM ANIMAL_INS A
+WHERE A.DATETIME > (SELECT DATETIME 
+                    FROM ANIMAL_OUTS 
+                    WHERE ANIMAL_ID = A.ANIMAL_ID)
+ORDER BY DATETIME;
